4fe0361783da866630dd712de88a63be84786c1a
briimiin <kipkorirbrimin005@gmail.com>
2024-03-22 23:53:24 +0300

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+//question1
+let names = ["Charlie", "Samip", "Ali"];
+let eventName = "surprise";
+
+function writeCards(names, eventName) {
+  let messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return messages;
+}
+
+let cards = writeCards(names, eventName);
+console.log(cards);
+//question2
+function countDown(startingNumber) {
+    let currentNumber = startingNumber;
+  
+    while (currentNumber >= 0) {
+      console.log(currentNumber);
+      currentNumber--;
+    }
+  }
+  
+  countDown(10);
+  
